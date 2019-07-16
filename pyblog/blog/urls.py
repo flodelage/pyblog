99269b901a900b3cdc6d56af523d08b6d0ddac66
Florian Delage <delageflo@ymail.com>
2019-07-16 21:44:47 +0200

    add post detail url, template, view

--- pyblog/blog/urls.py
+++ pyblog/blog/urls.py
@@ -6,4 +6,5 @@ from blog import views
 urlpatterns = [
     path('posts/', views.post_list, name='home'),
     path('posts/<str:category_name>/', views.post_list, name='post-list'),
+    path('posts/detail/<int:post_id>/', views.post_detail, name='post-detail')
 ]
--- pyblog/blog/views.py
+++ pyblog/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from blog.models import Post
 from blog import model_helpers
@@ -17,3 +17,14 @@ def post_list(request, category_name=model_helpers.post_category_all.slug()):
     }
 
     return render(request, 'blog/post_list.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+
+    context = {
+        'navigation_items': navigation.navigation_items(navigation.NAV_POSTS),
+        'post': post,
+    }
+
+    return render(request, 'blog/post_detail.html', context)
