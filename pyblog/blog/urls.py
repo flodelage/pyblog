a83026bcc19a1a568b203d85c1f6ca65d85db9f2
Florian Delage <delageflo@ymail.com>
2019-07-17 15:49:13 +0200

    fix creation commentt message

--- pyblog/blog/urls.py
+++ pyblog/blog/urls.py
@@ -6,6 +6,6 @@ from blog import views
 urlpatterns = [
     path('posts/', views.post_list, name='home'),
     path('posts/<str:category_name>/', views.post_list, name='post-list'),
-    path('posts/detail/<int:post_id>/', views.post_detail, name='post-detail')
-    path('posts/detail/<int:post_id>/<str:message>/', views.post_detail, name='post-detail-message')
+    path('posts/detail/<int:post_id>/', views.post_detail, name='post-detail'),
+    path('posts/detail/<int:post_id>/<str:message>/', views.post_detail, name='post-detail-message'),
 ]
