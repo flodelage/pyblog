cc1ece45df16caa0cd186e9254f211ab12b07d6d
Florian Delage <delageflo@ymail.com>
2019-07-17 15:44:52 +0200

    add form input comment

--- pyblog/blog/urls.py
+++ pyblog/blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('posts/', views.post_list, name='home'),
     path('posts/<str:category_name>/', views.post_list, name='post-list'),
     path('posts/detail/<int:post_id>/', views.post_detail, name='post-detail')
+    path('posts/detail/<int:post_id>/<str:message>/', views.post_detail, name='post-detail-message')
 ]
--- pyblog/blog/views.py
+++ pyblog/blog/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
+from django.http import HttpResponseRedirect
 
 from blog.models import Post, Comment
 from blog import model_helpers
 from blog import navigation
+from blog.forms import CreateCommentForm
+
 
 
 def post_list(request, category_name=model_helpers.post_category_all.slug()):
@@ -19,17 +22,30 @@ def post_list(request, category_name=model_helpers.post_category_all.slug()):
     return render(request, 'blog/post_list.html', context)
 
 
-def post_detail(request, post_id):
+def post_detail(request, post_id, message=''):
     post = get_object_or_404(Post, pk=post_id)
     posts_same_category = Post.objects.filter(published=True, category=post.category)\
         .exclude(pk=post_id)
     comments = post.comments.exclude(status=Comment.STATUS_HIDDEN).order_by('created_at')
+
+    if request.method == 'POST':
+        comment_form = CreateCommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return HttpResponseRedirect(reverse('post-detail-message', args=[post.pk, "You're comment has been posted!"]) + '#comments' )
+    else:
+        comment_form = CreateCommentForm()
 
     context = {
         'navigation_items': navigation.navigation_items(navigation.NAV_POSTS),
         'post': post,
         'posts_same_category': posts_same_category,
         'comments': comments,
+        'comment_form': comment_form,
+        'message': message,
     }
 
     return render(request, 'blog/post_detail.html', context)
